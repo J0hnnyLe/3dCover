160b033a2afdb87dcbc02f931919c4029cc2dbaa
Johnny <84757077+JohnnyXDDD@users.noreply.github.com>
2022-06-09 01:49:58 +0800

    Add files via upload

---
+++ 3D-cover-and-mask-animation/src/data/content.js
@@ -0,0 +1,27 @@
+const CONTENT_INFO = {
+    island: {
+        h1: "Never Stop",
+        h2: "Explore The World",
+        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus officia maxime nobis, sequi quisquam deserunt numquam consequatur culpa, libero officiis alias reiciendis perspiciatis! Error molestias numquam hic sint adipisci eligendi."
+    },
+    spring: {
+        h1: "Spring",
+        h2: "Feel the new vitality",
+        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aut in molestiae, consequatur accusamus magnam necessitatibus explicabo qui vitae veritatis eligendi consectetur nam totam quasi alias fuga modi, inventore voluptas?"
+    },
+    summer: {
+        h1: "Summer",
+        h2: "Enjoy passionate time",
+        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod debitis minus facilis ab quis blanditiis optio sapiente eum maiores labore, tempora veritatis in odit iure similique, doloremque recusandae dolorem unde."
+    },
+    autumn: {
+        h1: "Autumn",
+        h2: "Experience without limits",
+        p: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus alias libero cupiditate repudiandae error quos recusandae? Aperiam, provident numquam rem nihil fugit voluptate voluptatum, tempore sunt totam, voluptatibus cupiditate tenetur!"
+    },
+    winter: {
+        h1: "Winter",
+        h2: "Enjoy peace and freedom",
+        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias similique ullam accusantium sunt et, incidunt recusandae esse unde rerum repellendus repudiandae nam in atque iste facilis, impedit cum vel at."
+    },
+}
